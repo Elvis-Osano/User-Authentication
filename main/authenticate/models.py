3b5e55977eaadf557b510a2191142fc75b251b59
ELVIS OSANO <you@example.com>
2021-08-19 22:50:39 +0300

    Modifications

--- main/authenticate/models.py
+++ main/authenticate/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-# # Create your models here.
-# class Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     dp = models.ImageField(
-#         upload_to='dp/', default='default.png', null=True, blank=True)
-#     contact = models.CharField(max_length=50)
-#     marital = models.BooleanField(default=False)
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    dp = models.ImageField(
+        upload_to='dp/', default='default.png', null=True, blank=True)
+    contact = models.CharField(max_length=50)
+    marital = models.BooleanField(default=False)
 
-#     def __str__(self):
-#         return self.user.username
+    def __str__(self):
+        return self.user.username
